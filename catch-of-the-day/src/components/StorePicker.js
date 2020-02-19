387d8e499dcd68bcbaadda9436cc7a2bc9ee55be
Libor Zak <libor.zak.zn@gmail.com>
2020-02-19 21:47:18 +0100

    Prvni nahrani

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <p>
+        Takže, v téhle komponentě prostě importuji React, vytvořím class
+        StorePicker, která mi vráti tenhle text a pak vyexportuji tuhle classu
+        jako default, takže když ji pak zavolám, tak se mi kdekoliv vypíše tento
+        text.
+      </p>
+    );
+  }
+}
+
+export default StorePicker;
