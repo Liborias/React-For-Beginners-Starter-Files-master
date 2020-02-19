659a943a0a72a97861c8ec994a8be54d00d05120
Libor Zak <libor.zak.zn@gmail.com>
2020-02-19 23:22:24 +0100

    return může vrátit i více položek, když se ví jak na to

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,12 +3,21 @@ import React from "react";
 class StorePicker extends React.Component {
   render() {
     return (
-      <p>
-        Takže, v téhle komponentě prostě importuji React, vytvořím class
-        StorePicker, která mi vráti tenhle text a pak vyexportuji tuhle classu
-        jako default, takže když ji pak zavolám, tak se mi kdekoliv vypíše tento
-        text.
-      </p>
+      <React.Fragment>
+        <p>
+          Takže, v téhle komponentě prostě importuji React, vytvořím class
+          StorePicker, která mi vráti tenhle text a pak vyexportuji tuhle classu
+          jako default, takže když ji pak zavolám, tak se mi kdekoliv vypíše
+          tento text.
+        </p>
+        {/*Když chci něco komentovat a je nutné  to dělat uvnitř tagu, vně tagu to nebude fungovat*/}
+        <form className="storeSelector">
+          <h2>Vlož položku</h2>
+          <input type="text" required placeholder="Název položky" />
+          {/*required vyhazuje hlášku "vyplňte prosím toto pole" když se pak zmáčkne button, kde je to asi provázané se submit*/}
+          <button type="submit">Najdi položku →</button>
+        </form>
+      </React.Fragment>
     );
   }
 }
