f20905f73e29bb7f2c5a18e7e6958ac1f039e7e8
Libor Zak <libor.zak.zn@gmail.com>
2020-02-24 21:00:08 +0100

    components Order, Header, Inventory

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,19 +4,19 @@ class StorePicker extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <form className="store-selector">
+          <h2>Please enter a store</h2>
+          <input type="text" required placeholder="Store Name" />
+          {/*required vyhazuje hlášku "vyplňte prosím toto pole" když se pak zmáčkne button, kde je to asi provázané se submit*/}
+          <button type="submit">VISIT STORE →</button>
+        </form>
+        {/*Když chci něco komentovat a je nutné  to dělat uvnitř tagu, vně tagu to nebude fungovat
         <p>
           Takže, v téhle komponentě prostě importuji React, vytvořím class
           StorePicker, která mi vráti tenhle text a pak vyexportuji tuhle classu
           jako default, takže když ji pak zavolám, tak se mi kdekoliv vypíše
           tento text.
-        </p>
-        {/*Když chci něco komentovat a je nutné  to dělat uvnitř tagu, vně tagu to nebude fungovat*/}
-        <form className="storeSelector">
-          <h2>Vlož položku</h2>
-          <input type="text" required placeholder="Název položky" />
-          {/*required vyhazuje hlášku "vyplňte prosím toto pole" když se pak zmáčkne button, kde je to asi provázané se submit*/}
-          <button type="submit">Najdi položku →</button>
-        </form>
+        </p>*/}
       </React.Fragment>
     );
   }
