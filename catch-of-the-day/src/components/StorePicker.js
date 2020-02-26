18bdc3e7073c7168cf99f930973c748ead23100b
Libor Zak <libor.zak.zn@gmail.com>
2020-02-26 22:02:39 +0100

    Events, Refs, and this Bindings (Constructor etc.)

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,16 +2,27 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  goToStore(event) {
+  myInput = React.createRef();
+
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // } takto pomocí constructoru se bindovala funkce s this dříve
+
+
+  goToStore = (event) => {
     event.preventDefault();
-    console.log("Going To Store");
+    console.log(this.goToStore);
   }
+  // console.log() odkazuje na výpis toho co je v this, tentokrát v this.goToStore, arrow function naváže funkci přímo na this, bez ní by bylo nutné provést složitý binding, který se dělával dříve
+
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore()}>
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
