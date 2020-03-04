c4f2e113be79990906408b6ec322043ceca8adb6
Libor Žák <libor.zak.zn@gmail.com>
2020-03-04 22:19:39 +0100

    Vytváření formuláře v Inventory a ukládání dat

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -10,8 +10,12 @@ class StorePicker extends React.Component {
   // } takto pomocí constructoru se bindovala funkce s this dříve
 
   goToStore = event => {
+    // 1. zastavíme opakovaní a stopneme si data
     event.preventDefault();
-    console.log(this.myInput.value.value); // toto nefunguje, Wesbosovi jo, Proč?
+    // 2. uložíme si aktuální data do storeName
+    const storeName = this.myInput.current.value; // this.myInput.value.value nefunguje, Wesbosovi jo, Proč? Protože to tak bylo ve staré verzi Reactu. Nová nahradila používá místo value current, asi aby se odlišilo js value od raktího
+    // 3. vnutníme datům v propsu nové údaje, takže se změní název adresy a tím se zase spustí router.js a přesměruje nás ze StorePicker.js na App.js (je to možné proto, že StorePicker je child Routeru)
+    this.props.history.push(`/store/${storeName}`); //zpětné uvozovky se dělají pravý Alt + ý
   };
   // console.log() odkazuje na výpis toho co je v this, tentokrát v this.goToStore, arrow function naváže funkci přímo na this, bez ní by bylo nutné provést složitý binding, který se dělával dříve
 
